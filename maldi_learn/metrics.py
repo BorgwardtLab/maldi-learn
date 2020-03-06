ff18ec8d16e92753268021367aa9accbd7d0a3da
Caroline Weis <caroline.weis@bsse.ethz.ch>
2020-03-06 15:12:32 +0100

    add major_error_score and documentation

--- maldi_learn/metrics.py
+++ maldi_learn/metrics.py
@@ -1,5 +1,11 @@
 """
 Implement own metric functions.
+
+very_major_error_score:
+    Calculates very major error, also called false positive rate.
+
+major_error_score:
+    Calculates major error, also called false negative rate.
 """
 
 from sklearn.metrics import confusion_matrix
@@ -8,3 +14,6 @@ def very_major_error_score(y_true, y_pred, labels=[0,1]):
     TN, FP, FN, TP = confusion_matrix(y_true, y_pred, labels=labels).ravel()
     return FP / float(FP+TN)
 
+def major_error_score(y_true, y_pred, labels=[0,1]):
+    TN, FP, FN, TP = confusion_matrix(y_true, y_pred, labels=labels).ravel()
+    return FN / float(FN+TP)
