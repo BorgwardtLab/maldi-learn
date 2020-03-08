82783852c6e64530089a8c3a360ef0a2c3e7f9ce
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-03-08 16:43:39 +0100

    Extended stratification example

--- examples/stratification.py
+++ examples/stratification.py
@@ -17,19 +17,18 @@ explorer = DRIAMSDatasetExplorer(DRIAMS_ROOT)
 driams_dataset = load_driams_dataset(
             explorer.root,
             'DRIAMS-A',
-            ['2015'],
+            ['2015', '2017'],
             '*',
             ['5-Fluorocytosin', 'Ciprofloxacin'],
             encoder=DRIAMSLabelEncoder(),
             handle_missing_resistance_measurements='remove_if_all_missing',
-            nrows=1000
+            nrows=4000,
 )
-
-print(driams_dataset.y['5-Fluorocytosin'])
 
 # train-test split
 train_index, test_index = stratify_by_species_and_label(
-    driams_dataset.y, antibiotic='5-Fluorocytosin'
+    driams_dataset.y, antibiotic='5-Fluorocytosin',
+    remove_invalid=True,
 )
 
 print(train_index)
