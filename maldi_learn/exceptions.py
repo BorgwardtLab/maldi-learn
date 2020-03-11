66fab180a82d2e2ceb93439c6c83ba3004ae6d24
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-03-11 10:10:16 +0100

    Started preparing exceptions and warnings
    This will be useful when debugging behaviour in more scenarios. There
    are too many categories of things that might go wrong, so it is good
    to be prepared.

---
+++ maldi_learn/exceptions.py
@@ -0,0 +1,45 @@
+"""Custom exceptions for `maldi-learn`."""
+
+import warnings
+
+
+class SpeciesNotFoundException(ValueError):
+    """Exception to raise if a species was not found."""
+
+
+class SpeciesNotFoundWarning(UserWarning):
+    """Warning to show if a species was not found."""
+
+
+def _raise_or_warn(exception, warning, message, behaviour):
+    """Raise exception or show warning.
+
+    Raises a pre-defined exception or shows a pre-defined warning,
+    depending on the desired behaviour. This is a utility function that
+    should be used within `maldi-learn` functions to signal problems to
+    clients and users.
+
+    Parameters
+    ----------
+    exception
+        Exception to raise if `behaviour` is 'warn' or 'warning'. Should
+        be a class name.
+
+    warning
+        Warning to show if `behaviour` is 'warn' or 'warning'. Should be
+        a class name.
+
+    message : str
+        Message to use when raising an exception or showing a warning.
+
+    behaviour : str
+        Either one of 'raise', 'warn', or 'warning', to indicate the
+        desired behaviour, i.e. whether to raise an exception or not.
+    """
+
+    assert behaviour in ['raise', 'warn', 'warning']
+
+    if behaviour == 'raise':
+        raise exception(message=message)
+    else:
+        warnings.warn(message, category=warning)
