2d035f017a11a7d3cbe474a205c4b93e8fdd0595
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-03-11 09:21:04 +0100

    Added simple debug script for assessing issues with data set loading

---
+++ utilities/debug.py
@@ -0,0 +1,21 @@
+import dotenv
+import os
+
+from maldi_learn.driams import DRIAMSDatasetExplorer
+from maldi_learn.driams import DRIAMSLabelEncoder
+from maldi_learn.driams import load_driams_dataset
+
+dotenv.load_dotenv()
+DRIAMS_ROOT = os.getenv('DRIAMS_ROOT')
+
+explorer = DRIAMSDatasetExplorer(DRIAMS_ROOT)
+
+driams_dataset = load_driams_dataset(
+            explorer.root,
+            'DRIAMS-A',
+            ['2015', '2016', '2017', '2018'],
+            '*',
+            ['Trimetohprim-Sulfamethoxazole'],
+            encoder=DRIAMSLabelEncoder(),
+            handle_missing_resistance_measurements='remove_if_all_missing',
+)
