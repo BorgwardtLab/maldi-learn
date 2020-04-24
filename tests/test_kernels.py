3f9caa49071dc7cb9d91b4a605026a4b86852d98
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-04-24 14:54:44 +0200

    Extended test case for kernels

--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -34,22 +34,61 @@ MOCK_DATA = [
     ),
 ]
 
+MOCK_DATA_DIFFERENT_LENGTHS = [
+    MaldiTofSpectrum(
+        [[0.0,   5.0],
+         [10.7,  8.0],
+         [150.4, 10.],
+         [1000,  3.0]
+         ]
+    ),
+    MaldiTofSpectrum(
+        [[0.0,   15.0],
+         [10.7,  0.0],
+         [150.4, 10.],
+         [1000,  3.0]
+         ]
+    ),
+    MaldiTofSpectrum(
+        [[0.0, 0.0],
+         [333,  47.0],
+         [666,  23.0],
+         [888,  42.],
+         [999,  5.0]
+         ]
+    ),
+]
+
 MOCK_LABELS = [0, 0, 1]
 
 
 class TestPIKE(unittest.TestCase):
-    def test_fit(self):
-        clf = GaussianProcessClassifier(kernel=DiffusionKernel())
 
-        clf.fit(MOCK_DATA, MOCK_LABELS)
+    def _clf_smoke_test(self, X, y, expected_sigma):
+        clf = GaussianProcessClassifier(kernel=DiffusionKernel())
+        clf.fit(X, y)
 
         # These are just a 'smoke test' in the sense that we want to
         # figure out whether these operations can be performed.
-        clf.predict(MOCK_DATA)
-        clf.predict_proba(MOCK_DATA)
+        clf.predict(X)
+        clf.predict_proba(X)
 
         self.assertAlmostEqual(
             clf.kernel_.sigma,
-            2.1993352,
+            expected_sigma,
             places=6
+        )
+
+    def test_equal_lengths(self):
+        self._clf_smoke_test(
+            MOCK_DATA,
+            MOCK_LABELS,
+            2.1993352
+        )
+
+    def test_different_lengths(self):
+        self._clf_smoke_test(
+            MOCK_DATA_DIFFERENT_LENGTHS,
+            MOCK_LABELS,
+            2.1993352
         )
