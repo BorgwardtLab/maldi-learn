98135fe9bbdf6520446db98615f2a579d820ed49
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2020-05-18 21:12:09 +0200

    Extended binning script for new `DRIAMS` sites

--- utilities/bin_driams.sh
+++ utilities/bin_driams.sh
@@ -6,7 +6,7 @@
 # We want `6000` to finish first for all sites before starting with
 # another bin size.
 for BINS in 6000 3600; do
-  for SITE in "DRIAMS-A" "DRIAMS-B" "DRIAMS-C" "DRIAMS-D"; do
+  for SITE in "DRIAMS-A" "DRIAMS-B" "DRIAMS-C" "DRIAMS-D" "DRIAMS-E" "DRIAMS-F"; do
     ROOT=/links/groups/borgwardt/Data/DRIAMS/$SITE/id
     echo $SITE
     for DIRECTORY in $ROOT/*; do
